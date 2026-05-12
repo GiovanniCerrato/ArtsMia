abd839e15ae6848c55052a09ab75f99bb92fd069
giova <giovacer05@gmail.com>
2026-05-12 10:05:26 +0200

    completato con anche la ricorsione

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import copy
+
 import networkx as nx
 
 from database.DAO import DAO
@@ -10,6 +12,41 @@ class Model:
         self._idMapAO  = {}
         for n in self._nodes:
             self._idMapAO[n.object_id] = n
+        self._optPath = []
+        self._optCost = 0
+
+    def getOptPath(self, source, lun):
+        parziale = [source]
+
+        for n in self._graph.neighbors(source):
+            if n.classification == parziale[-1].classification:
+                parziale.append(n)
+                self._ricorsione(parziale, lun)
+                parziale.pop() #backtracking
+        return self._optPath, self._optCost
+
+    def _ricorsione(self, parziale, lun):
+        if len(parziale)==lun:
+            #condizione di terminazione, allora parziale è lunga esattamente lun
+            #per cui verifico che questa parziale sia meglio del mio best
+            # (condizione di ottimalità), ed in ogni caso esco.
+            if self._costoPath(parziale) > self._optCost:
+                self._optCost = self._costoPath(parziale)
+                self._optPath = copy.deepcopy(parziale)
+            return
+
+        #se arrivo qui posso ancora aggiungere nodi
+        for n in self._graph.neighbors(parziale[-1]):
+            if parziale[-1].classification == n.classification:
+                parziale.append(n)
+                self._ricorsione(parziale, lun)
+                parziale.pop()
+    def _costoPath(self, path):
+        costo = 0
+        for i in range(0,len(path)-1):
+            costo += self._graph[path[i]][path[i+1]]["weight"]
+        return costo
+
 
     def getInfoCompConnessa(self, id_oggetto):
         #cercare la comp connessa che contiene id_oggetto
@@ -64,5 +101,8 @@ class Model:
         return len(self._graph.nodes)
     def getNumEdges(self):
         return len(self._graph.edges)
+
+    def getNodeFromId(self, id_oggetto):
+        return self._idMapAO[id_oggetto]
 
 
